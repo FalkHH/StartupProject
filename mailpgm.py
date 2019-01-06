06004d2ef50ea74629ef0b1ed24d874373c28ce7
FalkHH <dwabl@online.de>
2019-01-06 19:18:11 +0100

    Batchaufrufe in Python angepasst

--- mailpgm.py
+++ mailpgm.py
@@ -1,1 +1,8 @@
-echo "Test Inhalt text" | mail -s "Test Betreff Mail" falk.wollatz@web.de
+import subprocess
+
+command = 'echo "Test Inhalt text" | mail -s "Test Betreff Mail" falk.wollatz@web.de'
+result = subprocess.run([command], stdout=subprocess.PIPE)
+
+print(result.stdout)
+
+
