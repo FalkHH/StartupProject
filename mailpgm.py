ff7dfc416500cef991bdd362a415cc57399df22e
FalkHH <dwabl@online.de>
2019-01-06 19:46:40 +0100

    text mit Luftfeuchtigkeit

--- mailpgm.py
+++ mailpgm.py
@@ -1,9 +1,12 @@
 import subprocess
+from sense_hat import SenseHat
+sense = SenseHat()
+humidity = sense.get_humidity()
 
-#command = 'echo "Test Inhalt text" | mail -s "Test Betreff Mail" falk.wollatz@web.de'
-#result = subprocess.run([command], stdout=subprocess.PIPE)
-
-p1 = subprocess.Popen(['echo', '"Test Inhalt text"'], stdout=subprocess.PIPE)
+text = "Luftfeuchtigkeit: %s %%rH" % humidity
+sense.show_message(text)
+print(text)
+p1 = subprocess.Popen(['echo', text], stdout=subprocess.PIPE)
 p2 = subprocess.Popen(['mail', '-s', '"Test Betreff Mail"', 'falk.wollatz@web.de'], stdin=p1.stdout)
 
 print(p2.stdout)
