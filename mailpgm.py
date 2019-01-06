9c1d89902e6c337cd027bb6df612c6d159815a17
FalkHH <dwabl@online.de>
2019-01-06 19:38:35 +0100

    p1 und p2 entdeckt

--- mailpgm.py
+++ mailpgm.py
@@ -1,8 +1,11 @@
 import subprocess
 
-command = 'echo "Test Inhalt text" | mail -s "Test Betreff Mail" falk.wollatz@web.de'
-result = subprocess.run([command], stdout=subprocess.PIPE)
+#command = 'echo "Test Inhalt text" | mail -s "Test Betreff Mail" falk.wollatz@web.de'
+#result = subprocess.run([command], stdout=subprocess.PIPE)
 
-print(result.stdout)
+p1 = subprocess.Popen(['echo', '"Test Inhalt text"'], stdout=subprocess.PIPE)
+p2 = subprocess.Popen(['mail', '-s', '"Test Betreff Mail"', 'falk.wollatz@web.de'], stdin=p1.stdout)
+
+print(p2.stdout)
 
 
