375a0a718e7a7f3e49ff8023c213cce76224ac6d
FalkHH <dwabl@online.de>
2019-03-31 18:38:25 +0200

    Neues Programm erstellt

---
+++ Luftfeuchtigkeit.py
@@ -0,0 +1,38 @@
+import time                      # importieren einer gesamten Standard-Bilbliothek 
+import os
+from sense_hat import SenseHat   # importieren einer Klasse aus spezieller Bilbliothek
+sensor = SenseHat()              # Instanz vom Objektklasse erstellt
+
+
+
+def LuftfeuchtigkeitMessen(Dateiname="Feuchtigkeitsdaten"):    #Funktion nutzt Standard-Datei, falls sie ohne speziellem Parameter aufgerufen wird
+	"""Misst die Luftfeuchtigkeit der Umgebung und schreibt den Wert in eine CSV-Datei
+	"""
+	Feuchtigkeit = sensor.get_humidity()                  # misst die Luftfeuchtigkeit
+	Zeit = time.time()                                    # 'Zeitstempel als Wert
+	F_out  = open(Dateiname + ".csv", "a")                # Offenen einer Datei zum Datenanhängen
+	F_out.write("%s , %2.2f" % (Zeit,Feuchtigkeit) )      # Datei mit Werten s=Sting f=Floatingpoint beschreiben
+	F_out.close()                                         # Datei schließen  
+
+def DateiUmbenennen(Dateiname="Feuchtigkeitsdaten"):
+	"""Benennt eine Datei um, indem ein Zeitstempel hinten am Namen angehängt wird
+	"""
+	Zeit = time.now()     # Zeitstempel als STring
+	os.rename("%s.csv" % Dateiname,"%s_%s.csv" % (Dateiname,Zeit))
+
+Tag_alt = time.strftime("%d")     # aktuellen Monatstag festlegen
+i = 1
+while True:
+	LuftfeuchtigkeitMessen()
+	time.sleep(30)				# 30 Sekunden warten
+	Tag_neu = time.strftime("%d")   # Monatstag feststellen
+	if Tag_neu != Tag_alt :
+		DateiUmbenennen()
+		Tag_alt = Tag_neu
+	i++
+	if i > 10 :
+		break		# While-Scheife verlassen
+		
+		
+	
+
--- mailpgm.py
+++ mailpgm.py
@@ -7,7 +7,7 @@ text = "Luftfeuchtigkeit: %s %%rH" % humidity
 sense.show_message(text)
 print(text)
 p1 = subprocess.Popen(['echo', text], stdout=subprocess.PIPE)
-p2 = subprocess.Popen(['mail', '-s', '"Test Betreff Mail"', 'falk.wollatz@web.de'], stdin=p1.stdout)
+p2 = subprocess.Popen(['mail', '-s', '"aktuelle Luftfeuchtigkeit"', 'falk.wollatz@web.de'], stdin=p1.stdout)
 
 print(p2.stdout)
 
