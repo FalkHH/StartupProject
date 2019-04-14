304b9e36b9fef853c667773c2f9fc1f0d191a377
FalkHH <dwabl@online.de>
2019-04-14 18:41:11 +0200

    MesswertSpeichern

--- Luftfeuchtigkeit.py
+++ Luftfeuchtigkeit.py
@@ -16,12 +16,12 @@ def LuftfeuchtigkeitMessen(Dateiname="Feuchtigkeitsdaten"):    #Funktion nutzt S
 	sensor.show_message("%2.2f %%rH * %2.2f" % (Feuchtigkeit,Feuchtigkeit) )      # %% ermoeglicht die Anzeige eines  %
 	MesswertSpeichern(Feuchtigkeit)	
 
-def MesswertSpeichern(m)		# schiebt die bislang gespeicherten Werte im Array nach vorne und speichert den neuen Wert ans Ende
+def MesswertSpeichern(Messwert)		# schiebt die bislang gespeicherten Werte im Array nach vorne und speichert den neuen Wert ans Ende
 	for( i = 1; i < 7; i++):
 		a[i] = a[i+1]
 		i += 1
 		if i = 7:
-			a[7] = m
+			a[7] = Messwert
 
 j = 1
 while True:
